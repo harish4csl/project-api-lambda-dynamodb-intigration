110ab85e9951735fe9909a88b6ca7bc54a26fca5
harish <122397227+harish4csl@users.noreply.github.com>
2026-02-07 19:56:11 +0530

    Fix case sensitivity in SQL insert statement

--- lambda_function.py
+++ lambda_function.py
@@ -47,7 +47,7 @@ def page_router(httpmethod, querystring, formbody):
 def insert_record(formbody):
     formbody = formbody.replace("=", "' : '")
     formbody = formbody.replace("&", "', '")
-    formbody = "INSERT INTO HARISH value {'" + formbody + "'}"
+    formbody = "INSERT INTO harish value {'" + formbody + "'}"
 
     client = boto3.client('dynamodb')
     response = client.execute_statement(Statement=formbody)
